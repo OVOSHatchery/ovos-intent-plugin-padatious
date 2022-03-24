f293338b149075776bf556e5826b548c2b5f098c
jarbasai <jarbasai@mailfence.com>
2022-03-24 14:10:23 +0000

    :tada:

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+PLUGIN_ENTRY_POINT = 'ovos-intent-plugin-padatious=ovos_intent_plugin_padatious:PadatiousExtractor'
+setup(
+    name='ovos-intent-plugin-padatious',
+    version='0.0.1',
+    description='A intent plugin for mycroft',
+    url='https://github.com/OpenVoiceOS/ovos-intent-plugin-padatious',
+    author='JarbasAi',
+    author_email='jarbasai@mailfence.com',
+    license='Apache-2.0',
+    packages=['ovos_intent_plugin_padatious'],
+    install_requires=["ovos-plugin-manager", "padatious"],
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Text Processing :: Linguistic',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    entry_points={'intentbox.intent': PLUGIN_ENTRY_POINT}
+)
